4cac5899e87ea1d57ba178dc82c8c332f6a77e06
jimoh yusu <expensive7832@gmail.com>
2023-10-15 16:47:14 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
+import NotFound from './Pages/NotFound'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+       <Route path="/" element={ <Home/> }/>
+       <Route path="/login" element={ <Login/> }/>
+      <Route path='/signup' element={ <Signup/> } />
+      <Route path='*' element={ <NotFound/> } />
+      </Routes>
+      <ToastContainer
+        position='top-center'
+      />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
